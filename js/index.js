5ce98de9c1be1b5978a3614c54f75783fd12f1a7
Victoria Aguero <victoriaaguerocm@gmail.com>
2025-02-06 14:54:15 -0300

    Agregué fotos y cosas muy generales en cada página, aún no las ordene

--- js/index.js
+++ js/index.js
@@ -47,6 +47,18 @@ function mostrarCarta() {
 document.querySelector('#botoncartadeldia').addEventListener('click', mostrarCarta);
 
 
+//MENU RESPONSIVE
+const menucelu = document.querySelector("#menucelu");
+const menucompu = document.querySelector("#menucompu");
+
+   iconomenu.addEventListener("click", () => {
+       //Creamos la funcion para ocultar o mostrar segun como esta 
+       if (menucelu.style.display === "block") {
+           menucompu.style.display = "none"; // Oculta el menú compu
+       } else {
+           menucompu.style.display = "block"; // Muestra el menú compu
+       }
+   });
 
 
 //MENU RESPONSIVE
